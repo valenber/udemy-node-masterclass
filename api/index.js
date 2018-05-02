48bbba912b213ecc9d2707130bf956269310b3d8
Valentin Berlin <valentin.berlin@gmail.com>
2018-05-03 00:08:10 +0200

    commit2

--- api/index.js
+++ api/index.js
@@ -1,14 +1,27 @@
-// Primary file for the API
+// Main file for the API
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
-// Respond to all requests with a string
+// Handle requests
 const server = http.createServer((req, res) => {
-  res.end('Hey there\n')
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get path from the URL
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send reponse
+  res.end('Hey there\n');
+
+  // Log path
+  console.log(`Received a request on path: ${trimmedPath}`);
+
 })
 
-// Start the server and listen on port 4000\
+// Start the server and listen on port 3000
 server.listen(3000, () => {
   console.log('Server is listening at port 3000 now')
 })
