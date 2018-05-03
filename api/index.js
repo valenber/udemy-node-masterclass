1379f5e62e3768d899240609b88e130f3e921b6f
Valentin Berlin <valentin.berlin@gmail.com>
2018-05-03 14:03:02 +0200

    commit3

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // Handle requests
 const server = http.createServer((req, res) => {
@@ -13,12 +14,31 @@ const server = http.createServer((req, res) => {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+  // Get query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the headers and an object
+  const headers = req.headers;
+
+  // Get the HTTP method
+  const method = req.method.toLowerCase();
+
   // Send reponse
   res.end('Hey there\n');
 
-  // Log path
-  console.log(`Received a request on path: ${trimmedPath}`);
+  //Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', data => {
+    buffer += decoder.write(data);
+  });
 
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    // Log request info
+    console.log('Request payload: ', buffer);
+  });  
 })
 
 // Start the server and listen on port 3000
